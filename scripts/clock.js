9b9e91a41b90ec77a61126c22496e8ec2a84d166
LizardButts <taylorc.raines@gmail.com>
2025-09-29 12:12:36 -0700

    Added UTC functionality to the clock

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,11 +1,25 @@
     function startTime() {
+        // Local Time
         const today = new Date();
         let h = today.getHours();
+        let h0 = today.getUTCHours();
         let m = today.getMinutes();
+        let m0 = today.getUTCMinutes();
         let s = today.getSeconds();
+        let s0 = today.getUTCSeconds();
         m = checkTime(m);
         s = checkTime(s);
-        document.getElementById('txtClock').innerHTML =  h + ":" + m + ":" + s;
+        m = checkTime(m0);
+        s = checkTime(s0);
+        document.getElementById('localClock').innerHTML = "Local: " + h + ":" + m + ":" + s + ",     ";
+
+        // UTC Time
+        const today0 = today.toUTCString();
+        document.getElementById('UTC-Clock').innerHTML = "UTC: " + h0 + ":" + m0 + ":" + s0 + ",     ";
+
+
+
+
         setTimeout(startTime, 1000);
         }
 
